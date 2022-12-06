84a79649adec2664c0d86b3e523faad0112aaa5f
reece-elder <reece.elder@qa.com>
2022-12-06 13:27:15 +0000

    this is the one

--- new_script.sh
+++ new_script.sh
@@ -1,3 +1,11 @@
 #! /bin/bash
 
-docker run --rm hello-world
+# Removes all images
+docker rmi -f $(docker images -aq)
+
+# Removing all containers
+docker rm -vf $(docker ps -aq)
+
+docker build -t Jenkins/003-Basic_pipeline flask-app
+docker run -d --network task-1 --name flask-app flask-app
+docker run -d -p 80:80 --network --mount type=bind,source=Jenkins/003-Basic_pipeline/nginx.conf,target=/etc/nginx/nginx.conf task-1 nginx
