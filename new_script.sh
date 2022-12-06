91d07f24ec3b511168665fb05ba0312f05606999
reece-elder <reece.elder@qa.com>
2022-12-06 13:33:14 +0000

    adding slash

--- new_script.sh
+++ new_script.sh
@@ -6,6 +6,6 @@ docker rmi -f $(docker images -aq)
 # Removing all containers
 docker rm -vf $(docker ps -aq)
 
-docker build -t flask-app Jenkins/003-Basic_pipeline 
+docker build -t flask-app /Jenkins/003-Basic_pipeline 
 docker run -d --network task-1 --name flask-app flask-app
 docker run -d -p 80:80 --network --mount type=bind,source=$(pwd)/Jenkins/003-Basic_pipeline/nginx.conf,target=/etc/nginx/nginx.conf task-1 nginx
