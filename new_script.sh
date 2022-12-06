7151e6b44ff5cf9242c49dab971947fc385604e8
reece-elder <reece.elder@qa.com>
2022-12-06 13:32:10 +0000

    new one

--- new_script.sh
+++ new_script.sh
@@ -8,4 +8,4 @@ docker rm -vf $(docker ps -aq)
 
 docker build -t flask-app Jenkins/003-Basic_pipeline 
 docker run -d --network task-1 --name flask-app flask-app
-docker run -d -p 80:80 --network --mount type=bind,source=Jenkins/003-Basic_pipeline/nginx.conf,target=/etc/nginx/nginx.conf task-1 nginx
+docker run -d -p 80:80 --network --mount type=bind,source=$(pwd)/Jenkins/003-Basic_pipeline/nginx.conf,target=/etc/nginx/nginx.conf task-1 nginx
